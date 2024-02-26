318b5baacee964fb38159a1a1fe091e1a586f6e2
valek2225 <valgalkin22@gmail.com>
2024-02-26 15:35:22 +0300

    First commit

--- main.py
+++ main.py
@@ -3,6 +3,23 @@ import time
 import pygame
 from random import randrange as rnd
 
+def check(dx, dy, ball, rect):
+    if dx > 0:
+        d_x = ball.right - rect.left
+    else:
+        d_x = rect.right - ball.left
+    if dy > 0:
+        d_y = ball.bottom - rect.top
+    else:
+        d_y = rect.bottom - ball.top
+    if abs(d_x - d_y) < 10:
+        dx, dy = -dx, -dy
+    elif d_x > d_y:
+        dy = -dy
+    elif d_y > d_x:
+        dx = -dx
+    return dx, dy
+
 size = w, h = 1200, 800
 fps = 100
 # start
@@ -23,19 +40,19 @@ while True:
     if flag:
         break
 # экран и отступы
-paddle_w = 330
-paddle_h = 35
-paddle_speed = 15
-paddle = pygame.Rect(w // 2 - paddle_w // 2, h - paddle_h - 10, paddle_w, paddle_h)
+pw = 330
+ph = 35
+ps = 15
+p = pygame.Rect(w // 2 - pw // 2, h - ph - 10, pw, ph)
 # мяч
-ball_radius = 20
-ball_speed = 6
-ball_rect = int(ball_radius * 2 ** 0.5)
-ball = pygame.Rect(rnd(ball_rect, w - ball_rect), h // 2, ball_rect, ball_rect)
+br = 20
+bs = 6
+b_r = int(br * 2 ** 0.5)
+b = pygame.Rect(rnd(b_r, w - b_r), h // 2, b_r, b_r)
 dx, dy = 1, -1
 # блоки
-block_list = [pygame.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
-color_list = [(rnd(30, 256), rnd(30, 256), rnd(30, 256)) for i in range(10) for j in range(4)]
+bl = [pygame.Rect(10 + 120 * i, 10 + 70 * j, 100, 50) for i in range(10) for j in range(4)]
+cl = [(rnd(30, 256), rnd(30, 256), rnd(30, 256)) for i in range(10) for j in range(4)]
 clock = pygame.time.Clock()
 
 # main
@@ -52,25 +69,6 @@ game_lose = pygame.transform.scale(game_lose, size)
 heals = 3
 
 
-def detect_collision(dx, dy, ball, rect):
-    if dx > 0:
-        delta_x = ball.right - rect.left
-    else:
-        delta_x = rect.right - ball.left
-    if dy > 0:
-        delta_y = ball.bottom - rect.top
-    else:
-        delta_y = rect.bottom - ball.top
-
-    if abs(delta_x - delta_y) < 10:
-        dx, dy = -dx, -dy
-    elif delta_x > delta_y:
-        dy = -dy
-    elif delta_y > delta_x:
-        dx = -dx
-    return dx, dy
-
-
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -83,30 +81,30 @@ while True:
     if heals == 3:
         sc.blit(heart_image, (w - 64 - 1, h - 4 * 64 - 1))
     # создание карты
-    [pygame.draw.rect(sc, color_list[color], block) for color, block in enumerate(block_list)]
+    [pygame.draw.rect(sc, cl[c], b) for c, b in enumerate(bl)]
     pygame.draw.rect(sc, pygame.Color('darkorange'), paddle)
-    pygame.draw.circle(sc, pygame.Color('blue'), ball.center, ball_radius)
+    pygame.draw.circle(sc, pygame.Color('blue'), ball.center, br)
     # движение мяча
-    ball.x += ball_speed * dx
-    ball.y += ball_speed * dy
-    if ball.centerx < ball_radius or ball.centerx > w - ball_radius:
+    b.x += bs * dx
+    b.y += bs * dy
+    if b.centerx < br or b.centerx > w - br:
         dx = -dx
-    if ball.centery < ball_radius:
+    if b.centery < br:
         dy = -dy
     # столкновение
-    if ball.colliderect(paddle) and dy > 0:
-        dx, dy = detect_collision(dx, dy, ball, paddle)
-    hit_index = ball.collidelist(block_list)
-    if hit_index != -1:
-        hit_rect = block_list.pop(hit_index)
-        hit_color = color_list.pop(hit_index)
-        dx, dy = detect_collision(dx, dy, ball, hit_rect)
-        pygame.draw.rect(sc, hit_color, hit_rect)
+    if dy > 0 and b.colliderect(p):
+        dx, dy = check(dx, dy, b, p)
+    hi = b.collidelist(bl)
+    if hi != -1:
+        hr = bl.pop(hi)
+        hc = cl.pop(hi)
+        dx, dy = check(dx, dy, b, hr)
+        pygame.draw.rect(sc, hc, hr)
         fps += 2
-    if ball.bottom > h:
+    if b.bottom > h:
         heals -= 1
-        ball.x = 1100
-        ball.y = 400
+        b.x = 1100
+        b.y = 400
         if heals == 0:
             sc.fill((255, 255, 255))
             pygame.display.flip()
@@ -118,7 +116,7 @@ while True:
                         exit()
                 time.sleep(0.01)
             exit()
-    elif not len(block_list):
+    elif not len(bl):
         sc.fill((255, 255, 255))
         sc.blit(game_won, (0, 0))
         pygame.display.flip()
@@ -130,9 +128,9 @@ while True:
         exit()
     # управление платформой
     key = pygame.key.get_pressed()
-    if (key[pygame.K_LEFT] or key[pygame.K_a]) and paddle.left > 0:
-        paddle.left -= paddle_speed
-    if (key[pygame.K_RIGHT] or key[pygame.K_d]) and paddle.right < w:
-        paddle.right += paddle_speed
+    if (key[pygame.K_LEFT] or key[pygame.K_a]) and p.left > 0:
+        p.left -= ps
+    if (key[pygame.K_RIGHT] or key[pygame.K_d]) and p.right < w:
+        p.right += ps
     pygame.display.flip()
     clock.tick(fps)
